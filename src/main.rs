2894ddd10ee7916bb1091baefc4ae91a00d3dce2
Thomas <thomasmartin98@hotmail.com>
2022-01-29 20:03:33 +0000

    slight refactor

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,47 @@ pub struct AppProps {
 
 #[function_component(App)]
 fn app() -> Html {
-    let counter = use_state(|| 0);
+    let row_index = use_state(|| 0);
+    let won = use_state(|| false);
+    let grid_state = use_state(|| vec![vec!["", "", "", "", ""]; 6]);
+
+    let test_word = "tests";
 
     html! {
         <div  class={classes!("app")}>
             <h1>{"RUST WORDLE"}</h1>
+
             <div class={classes!("words-container")}>
-                <Word_Row/>
-                <Word_Row/>
-                <Word_Row/>
-                <Word_Row/>
-                <Word_Row/>
+                {for grid_state.iter().map(|lettersVec|{html!{
+                    <Word_Row/>
+                }})}
+            </div>
+
+
+
+            <div>
+                <input type="text"/>
+                <button type="button"> {"Press to guess"} </button>
             </div>
 
         </div>
     }
+}
+
+// enum cell_types{
+//     CorrectPos="correctPos";
+//     incorrectPos="incorrectPos",
+//     incorrect="incorrect",
+// }
+
+pub struct word_obj {
+    pub cellType: String,
+    pub value: char,
+}
+
+#[derive(Properties, PartialEq)]
+pub struct word_row_props {
+    pub word_guessed: Vec<String>,
 }
 
 #[function_component(Word_Row)]
@@ -32,20 +58,6 @@ fn word_row() -> Html {
         <div class={classes!("word-row-container")}>
         {for word_guesses.iter().map(|letter|{html!{<div class={classes!("letter-container")}><p class={classes!("letter")}>{letter}</p></div>}})}
         </div>
-    }
-}
-
-#[function_component(Test)]
-fn test(props: &AppProps) -> Html {
-    let text = use_state(|| "test");
-
-    html! {
-        <div>
-            <p>{&props.words}</p>
-            <input type="text" value={*text}/>
-            <p>{*text}</p>
-        </div>
-
     }
 }
 
