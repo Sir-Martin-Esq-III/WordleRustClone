92fe316bd30deaac6a91e1319c7ccbfcdc3405ea
Thomas <thomasmartin98@hotmail.com>
2022-01-29 17:22:43 +0000

    basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use yew::prelude::*;
+use yew::{classes, html};
+
+#[derive(Properties, PartialEq)]
+pub struct AppProps {
+    pub words: String,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let counter = use_state(|| 0);
+
+    html! {
+        <div  class={classes!("app")}>
+            <h1>{"RUST WORDLE"}</h1>
+            <div class={classes!("words-container")}>
+                <Word_Row/>
+                <Word_Row/>
+                <Word_Row/>
+                <Word_Row/>
+                <Word_Row/>
+            </div>
+
+        </div>
+    }
+}
+
+#[function_component(Word_Row)]
+fn word_row() -> Html {
+    let word_guesses = use_state(|| vec!["t", "e", "s", "t", "s"]);
+    html! {
+        <div class={classes!("word-row-container")}>
+        {for word_guesses.iter().map(|letter|{html!{<div class={classes!("letter-container")}><p class={classes!("letter")}>{letter}</p></div>}})}
+        </div>
+    }
+}
+
+#[function_component(Test)]
+fn test(props: &AppProps) -> Html {
+    let text = use_state(|| "test");
+
+    html! {
+        <div>
+            <p>{&props.words}</p>
+            <input type="text" value={*text}/>
+            <p>{*text}</p>
+        </div>
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
