3d5d1e83c58faa3d1e86741748717d3a2c7160d6
Thomas <thomasmartin98@hotmail.com>
2022-02-05 23:21:03 +0000

    refactor to use a vec of strings instead of a nested vec

--- src/main.rs
+++ src/main.rs
@@ -25,16 +25,7 @@ fn app() -> Html {
     let row_index = 0;
     let won = use_state(|| false);
     let mut current_guess = use_state(|| "".to_string());
-    let test = use_state(|| {
-        vec![
-            "t".to_string(),
-            "t".to_string(),
-            "t".to_string(),
-            "t".to_string(),
-        ]
-    });
-    let grid_state = use_state(|| vec![vec!["\n".to_string(); 5]; 6]);
-
+    let grid_state = use_state(|| vec!["     ".to_string(); 6]);
     let oninput = {
         let current_guess = current_guess.clone();
         println!("here");
@@ -45,44 +36,29 @@ fn app() -> Html {
     };
 
     let onclick = {
-        let test = test.clone();
         let current_guess = current_guess.clone();
         let grid_state = grid_state.clone();
 
         Callback::from(move |e: MouseEvent| {
-            // let f: String = String::from("TESTS");
-            // let t = f.split("").collect();
-            // let l = current_guess
-            //     .split("")
-            //     .map(|letter| letter.to_string())
-            //     .collect();
-
-            // test.set(l)
-            let l = vec![vec!["G".to_string(); 5]; 6];
-            grid_state.set(l);
+            let l = current_guess.to_string();
+            let mut t = grid_state.clone().to_vec();
+            t[row_index] = l;
+            grid_state.set(t);
         })
     };
+
     let mut grid = grid_state
         .iter()
-        .map(|row: &Vec<String>| {
-            html! {<Word_Row word_guessed={row.clone()}/>}
+        .map(|word: &String| {
+            html! {<Word_Row word_guessed={word.clone()}/>}
         })
         .collect::<Vec<_>>();
 
     html! {
         <div  class={classes!("app")}>
             <h1>{"RUST WORDLE"}</h1>
-            <div class={classes!("words-container")}>
 
-            <h1>{&grid_state[0][0]}</h1>
-            <div>{grid}</div>
-            </div>
-            <div>
-            {for test.iter().map(|letter|{html!{
-               <h1>{letter}</h1>
-            }})}
-            </div>
-
+            <div class={classes!("words-container")}>{grid}</div>
             <div>
                 <input {oninput} type="text" minlength="5" maxlength="5"  value={{(*current_guess).clone()}}/>
                 <h1>{{(*current_guess).clone()}}</h1>
@@ -96,15 +72,22 @@ fn app() -> Html {
 
 #[derive(Properties, PartialEq)]
 pub struct word_row_props {
-    pub word_guessed: Vec<String>,
+    pub word_guessed: String,
 }
 #[function_component(Word_Row)]
 fn word_row(props: &word_row_props) -> Html {
+    let row = props
+        .word_guessed
+        .split_terminator("")
+        .skip(1)
+        .map(|letter| {
+            html! {<div class="letter-container"><p class="letter">{letter.to_uppercase()}</p></div>}
+        })
+        .collect::<Vec<_>>();
     println!("{:#?}", props.word_guessed);
-    let word_guesses = use_state(|| props.word_guessed.clone());
     html! {
         <div class={classes!("word-row-container")}>
-        {for word_guesses.iter().map(|letter|{html!{<div class="letter-container"><p class="letter">{letter}</p></div>}})}
+            {row}
         </div>
     }
 }
