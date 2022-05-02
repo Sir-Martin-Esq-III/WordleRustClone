03ae09e4090f0cd91587d96640eeb8f060f64cb7
Thomas <thomasmartin98@hotmail.com>
2022-05-02 13:01:21 +0100

    added reset functionality and a modal

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ enum cell_types {
     CORRECTPOS,
     INCORRECTPOS,
     INCORRECT,
+    EMPTY,
 }
 
 #[derive(Debug, Clone)]
@@ -20,7 +21,7 @@ pub struct AppProps {
     pub words: String,
 }
 
-fn determineCellType(word_to_guess: &str, letter: &char, idx: &usize) -> cell_types {
+fn deterimine_cell_type(word_to_guess: &str, letter: &char, idx: &usize) -> cell_types {
     let found = word_to_guess.find(*letter);
     match found {
         Some(found) => {
@@ -43,7 +44,7 @@ fn app() -> Html {
     let grid_state = use_state(|| {
         vec![
             grid_cell {
-                cell_type: cell_types::INCORRECT,
+                cell_type: cell_types::EMPTY,
                 cell_value: " ".to_string(),
             };
             30
@@ -56,19 +57,17 @@ fn app() -> Html {
             let cell_type = cell.cell_type.clone();
             let class_name: &str;
             match cell_type {
-                cell_types::INCORRECT => class_name = "letter-container",
-                cell_types::CORRECTPOS => class_name = "letter-container-correct",
-                cell_types::INCORRECTPOS => class_name = "letter-container-incorrect",
+                cell_types::INCORRECT => class_name = "letter-container-incorrect withval",
+                cell_types::CORRECTPOS => class_name = "letter-container-correct withval",
+                cell_types::INCORRECTPOS => class_name = "letter-container-incorrectPos withval",
+                _ => class_name = "letter-container",
             }
-
             html! {<div class={class_name}><p class="letter">{cell.cell_value.clone()}</p></div>}
         })
         .collect::<Vec<_>>();
 
     let oninput = {
-        println!("{:?}", *grid_state);
         let current_guess = current_guess.clone();
-        println!("here");
         Callback::from(move |e: InputEvent| {
             let input: HtmlInputElement = e.target_unchecked_into();
             current_guess.set(input.value());
@@ -77,10 +76,16 @@ fn app() -> Html {
 
     let handleguess = {
         let current_guess = current_guess.clone();
+        let won = won.clone();
+        let grid_state = grid_state.clone();
+        let row_index = row_index.clone();
         Callback::from(move |_| {
+            if *current_guess == *word_to_guess {
+                won.set(true)
+            }
             let mut t = grid_state.clone().to_vec();
             for (i, c) in current_guess.chars().enumerate() {
-                let cell_t = determineCellType(&word_to_guess, &c, &i);
+                let cell_t = deterimine_cell_type(&word_to_guess, &c, &i);
                 let l = grid_cell {
                     cell_type: cell_t,
                     cell_value: c.to_ascii_uppercase().to_string(),
@@ -89,6 +94,23 @@ fn app() -> Html {
             }
             row_index.set(*row_index + 1);
             grid_state.set(t);
+        })
+    };
+
+    let handelReset = {
+        let won = won.clone();
+        let row_index = row_index.clone();
+        let grid_state = grid_state.clone();
+        Callback::from(move |_: MouseEvent| {
+            won.set(false);
+            row_index.set(0);
+            grid_state.set(vec![
+                grid_cell {
+                    cell_type: cell_types::EMPTY,
+                    cell_value: " ".to_string(),
+                };
+                30
+            ]);
         })
     };
 
@@ -101,8 +123,28 @@ fn app() -> Html {
                 <input {oninput} type="text" minlength="5" maxlength="5"  value={{(*current_guess).clone()}}/>
                 <h1>{{(*current_guess).clone()}}</h1>
             </div>
-            <button onclick={handleguess} type="button"> {"Press to guess"} </button>
+            <button onclick={handleguess} disabled={*won} type="button"> {"Press to guess"} </button>
+            if *won==true{
+                <Modal reset={&handelReset}/>
+            }
 
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct modalProps {
+    pub reset: Callback<MouseEvent>,
+}
+
+#[function_component(Modal)]
+pub fn modal(props: &modalProps) -> Html {
+    html! {
+        <div class="modal">
+            <div class="content">
+                <h1>{"Congrats You won!"}</h1>
+                <button onclick={&props.reset}>{"click here to reset"}</button>
+            </div>
         </div>
     }
 }
